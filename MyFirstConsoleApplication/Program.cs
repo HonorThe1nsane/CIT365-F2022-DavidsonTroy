f69c2c40a3450d071e1a988b0643cc6a0c4aae37
Troy Davidson <troymd2009@hotmail.com>
2022-09-15 15:36:41 -0600

    update

--- MyFirstConsoleApplication/Program.cs
+++ MyFirstConsoleApplication/Program.cs
@@ -21,17 +21,17 @@ namespace MyFirstConsoleApplication
 
             Console.WriteLine("What it is your name? ");
             var personName = Console.ReadLine();
-            Console.WriteLine("Hi " + personName + "!" + " Where are you from?");
+            Console.WriteLine($"Hi {personName}! Where are you from?");
             var pLocation = Console.ReadLine();
             
             var person = new Person
             {
                 Name = personName,
                 Location = pLocation
-        };
+            };
 
 
-            Console.WriteLine("I have never been to " + person.Location + ". I bet it is a nice place. Press any key to continue...");
+            Console.WriteLine($"I have never been to {person.Location} I bet it is a nice place. Press any key to continue...");
         }
     }
 
