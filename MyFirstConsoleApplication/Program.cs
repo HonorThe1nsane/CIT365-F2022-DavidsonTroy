897ea2e49bca120050e2015206d1f8e780fa6b19
Troy Davidson <troymd2009@hotmail.com>
2022-09-15 13:33:23 -0600

    update

--- MyFirstConsoleApplication/Program.cs
+++ MyFirstConsoleApplication/Program.cs
@@ -10,16 +10,26 @@ namespace MyFirstConsoleApplication
     {
         static void Main()
         {
-            
+            //Get the user name and location
+            GetUserNameAndLocation();
+
+
         }
 
-        private void GetUserNameAndLocation()
+        private static void GetUserNameAndLocation()
         {
-            var person = new Person();
+
             Console.WriteLine("What it is your name? ");
-            person.Name = Console.ReadLine();
-            Console.WriteLine("HI " + person.Name + "!" + "Where are you from?");
-            person.Location = Console.ReadLine();
+            var personName = Console.ReadLine();
+            Console.WriteLine("Hi " + personName + "!" + " Where are you from?");
+            var pLocation = Console.ReadLine();
+            
+            var person = new Person
+            {
+                Name = personName,
+                Location = pLocation
+        };
+
 
             Console.WriteLine("I have never been to " + person.Location + ". I bet it is a nice place. Press any key to continue...");
         }
