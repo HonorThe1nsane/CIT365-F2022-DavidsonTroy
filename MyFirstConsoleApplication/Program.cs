d1c954cb1c13fa38d8ed3017feba8fbf796b78fc
Troy Davidson <troymd2009@hotmail.com>
2022-09-15 12:42:53 -0600

    Step 3 for requirements

--- MyFirstConsoleApplication/Program.cs
+++ MyFirstConsoleApplication/Program.cs
@@ -1,15 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace MyFirstConsoleApplication
+﻿namespace MyFirstConsoleApplication
 {
     internal class Program
     {
         static void Main()
         {
-        }
+        } 
+    }
+
+    private void GetUserNameAndLocation
+    {
+        var person = new Person();
+    person.Name = Console.ReadLine();
+        person.Location = Console.ReadLine();
     }
 }
