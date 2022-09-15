9747404dcb0165a0bebd640f32de4814cd8587ef
Troy Davidson <troymd2009@hotmail.com>
2022-09-15 12:51:00 -0600

    get user name and location function

--- MyFirstConsoleApplication/Program.cs
+++ MyFirstConsoleApplication/Program.cs
@@ -1,16 +1,29 @@
-﻿namespace MyFirstConsoleApplication
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstConsoleApplication
 {
     internal class Program
     {
         static void Main()
         {
-        } 
+            
+        }
+
+        private void GetUserNameAndLocation()
+        {
+            var person = new Person();
+            Console.WriteLine("What it is your name? ");
+            person.Name = Console.ReadLine();
+            Console.WriteLine("HI " + person.Name + "!" + "Where are you from?");
+            person.Location = Console.ReadLine();
+
+            Console.WriteLine("I have never been to " + person.Location + ". I bet it is a nice place. Press any key to continue...");
+        }
     }
 
-    private void GetUserNameAndLocation
-    {
-        var person = new Person();
-    person.Name = Console.ReadLine();
-        person.Location = Console.ReadLine();
-    }
+
 }
