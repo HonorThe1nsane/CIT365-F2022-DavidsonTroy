a1cd1bb78b70be6377463cf2f1cc888bd60b2590
Troy Davidson <troymd2009@hotmail.com>
2022-10-01 19:43:00 -0600

    fixed main form page

--- MegaDesk -Davidson/DeskQuote.Designer.cs
+++ MegaDesk -Davidson/DeskQuote.Designer.cs
@@ -28,84 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(DeskQuote));
-            this.newQuote = new System.Windows.Forms.Button();
-            this.viewQuote = new System.Windows.Forms.Button();
-            this.searchQuotes = new System.Windows.Forms.Button();
-            this.exit = new System.Windows.Forms.Button();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
-            // 
-            // newQuote
-            // 
-            this.newQuote.Location = new System.Drawing.Point(71, 50);
-            this.newQuote.Name = "newQuote";
-            this.newQuote.Size = new System.Drawing.Size(163, 51);
-            this.newQuote.TabIndex = 0;
-            this.newQuote.Text = "Add New Quote";
-            this.newQuote.UseVisualStyleBackColor = true;
-            // 
-            // viewQuote
-            // 
-            this.viewQuote.Location = new System.Drawing.Point(71, 127);
-            this.viewQuote.Name = "viewQuote";
-            this.viewQuote.Size = new System.Drawing.Size(163, 51);
-            this.viewQuote.TabIndex = 1;
-            this.viewQuote.Text = "View Quotes";
-            this.viewQuote.UseVisualStyleBackColor = true;
-            this.viewQuote.Click += new System.EventHandler(this.button2_Click);
-            // 
-            // searchQuotes
-            // 
-            this.searchQuotes.Location = new System.Drawing.Point(71, 213);
-            this.searchQuotes.Name = "searchQuotes";
-            this.searchQuotes.Size = new System.Drawing.Size(163, 51);
-            this.searchQuotes.TabIndex = 2;
-            this.searchQuotes.Text = "Search Quotes";
-            this.searchQuotes.UseVisualStyleBackColor = true;
-            // 
-            // exit
-            // 
-            this.exit.Location = new System.Drawing.Point(71, 299);
-            this.exit.Name = "exit";
-            this.exit.Size = new System.Drawing.Size(163, 51);
-            this.exit.TabIndex = 3;
-            this.exit.Text = "Exit";
-            this.exit.UseVisualStyleBackColor = true;
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.InitialImage")));
-            this.pictureBox1.Location = new System.Drawing.Point(364, 50);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(313, 299);
-            this.pictureBox1.TabIndex = 4;
-            this.pictureBox1.TabStop = false;
             // 
             // DeskQuote
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.pictureBox1);
-            this.Controls.Add(this.exit);
-            this.Controls.Add(this.searchQuotes);
-            this.Controls.Add(this.viewQuote);
-            this.Controls.Add(this.newQuote);
             this.Name = "DeskQuote";
-            this.Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.Text = "MainMenu - Davidson";
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Button newQuote;
-        private System.Windows.Forms.Button viewQuote;
-        private System.Windows.Forms.Button searchQuotes;
-        private System.Windows.Forms.Button exit;
-        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
--- MegaDesk -Davidson/MainMenu.Designer.cs
+++ MegaDesk -Davidson/MainMenu.Designer.cs
@@ -28,13 +28,87 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainMenu));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.exit = new System.Windows.Forms.Button();
+            this.searchQuotes = new System.Windows.Forms.Button();
+            this.viewQuote = new System.Windows.Forms.Button();
+            this.newQuote = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.ImageLocation = "C:\\Users\\troym\\source\\repos\\CIT365-F2022-DavidsonTroy\\MegaDesk -Davidson\\images\\p" +
+    "exels-ingo-joseph-631411 (2).jpg";
+            this.pictureBox1.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.InitialImage")));
+            this.pictureBox1.Location = new System.Drawing.Point(445, 130);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(233, 193);
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.WaitOnLoad = true;
+            // 
+            // exit
+            // 
+            this.exit.Location = new System.Drawing.Point(122, 324);
+            this.exit.Name = "exit";
+            this.exit.Size = new System.Drawing.Size(163, 51);
+            this.exit.TabIndex = 8;
+            this.exit.Text = "Exit";
+            this.exit.UseVisualStyleBackColor = true;
+            // 
+            // searchQuotes
+            // 
+            this.searchQuotes.Location = new System.Drawing.Point(122, 238);
+            this.searchQuotes.Name = "searchQuotes";
+            this.searchQuotes.Size = new System.Drawing.Size(163, 51);
+            this.searchQuotes.TabIndex = 7;
+            this.searchQuotes.Text = "Search Quotes";
+            this.searchQuotes.UseVisualStyleBackColor = true;
+            // 
+            // viewQuote
+            // 
+            this.viewQuote.Location = new System.Drawing.Point(122, 152);
+            this.viewQuote.Name = "viewQuote";
+            this.viewQuote.Size = new System.Drawing.Size(163, 51);
+            this.viewQuote.TabIndex = 6;
+            this.viewQuote.Text = "View Quotes";
+            this.viewQuote.UseVisualStyleBackColor = true;
+            // 
+            // newQuote
+            // 
+            this.newQuote.Location = new System.Drawing.Point(122, 75);
+            this.newQuote.Name = "newQuote";
+            this.newQuote.Size = new System.Drawing.Size(163, 51);
+            this.newQuote.TabIndex = 5;
+            this.newQuote.Text = "Add New Quote";
+            this.newQuote.UseVisualStyleBackColor = true;
+            // 
+            // MainMenu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.exit);
+            this.Controls.Add(this.searchQuotes);
+            this.Controls.Add(this.viewQuote);
+            this.Controls.Add(this.newQuote);
+            this.Name = "MainMenu";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button exit;
+        private System.Windows.Forms.Button searchQuotes;
+        private System.Windows.Forms.Button viewQuote;
+        private System.Windows.Forms.Button newQuote;
     }
 }
 
