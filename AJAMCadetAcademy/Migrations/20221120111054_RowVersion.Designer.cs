6f28e71341a257adae7cbd15e3ace44e6968f3d5
Troy Davidson <troymd2009@hotmail.com>
2022-11-20 04:13:13 -0700

    Added Deptarments Controller

--- AJAMCadetAcademy/Migrations/20221120111054_RowVersion.Designer.cs
+++ AJAMCadetAcademy/Migrations/20221120111054_RowVersion.Designer.cs
@@ -4,14 +4,16 @@ using AJAMCadetAcademy.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace AJAMCadetAcademy.Migrations
 {
     [DbContext(typeof(SchoolContext))]
-    partial class SchoolContextModelSnapshot : ModelSnapshot
+    [Migration("20221120111054_RowVersion")]
+    partial class RowVersion
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
