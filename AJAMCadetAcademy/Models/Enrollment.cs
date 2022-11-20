fdbf13f63a34881b3ebf996eb394be5f564281ac
Troy Davidson <troymd2009@hotmail.com>
2022-11-20 02:34:07 -0700

    updated enrollment

--- AJAMCadetAcademy/Models/Enrollment.cs
+++ AJAMCadetAcademy/Models/Enrollment.cs
@@ -1,4 +1,7 @@
-﻿namespace AJAMCadetAcademy.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AJAMCadetAcademy.Models
 {
     
         public enum Grade
@@ -11,7 +14,8 @@
             public int EnrollmentID { get; set; }
             public int CourseID { get; set; }
             public int StudentID { get; set; }
-            public Grade? Grade { get; set; }
+        [DisplayFormat(NullDisplayText = "No grade")]
+        public Grade? Grade { get; set; }
 
             public Course Course { get; set; }
             public Student Student { get; set; }
