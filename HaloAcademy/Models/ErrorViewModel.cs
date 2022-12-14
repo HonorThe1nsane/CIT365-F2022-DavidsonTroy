8b4e8070aadb97669bcc1612d6a6ad68be3c60bc
Troy Davidson <troymd2009@hotmail.com>
2022-12-14 11:37:10 -0700

    Deleted HaloAcademy Need to do it in 5.0 .NET

--- HaloAcademy/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace HaloAcademy.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
